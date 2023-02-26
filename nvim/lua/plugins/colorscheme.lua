c7197a429292693e679d37cc1be570828423cdde
Xanadu Zhang <teaeml@gmail.com>
2023-02-26 22:10:55 +0800

    init the colorscheme `chengdu-west-moon` based on `tokyonight-moon`

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,13 @@ return {
   -- add gruvbox & papercolor
   { "lifepillar/vim-gruvbox8" },
   { "gongxgong/papercolor-theme" },
+  { "yeahnoob/chengdu-west.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox8",
+      colorscheme = "chengdu-west-moon",
     },
   },
 }
