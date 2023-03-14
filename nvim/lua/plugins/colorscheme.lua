12b8741c9486ab40cb9d16082b615099dda5da5d
yeahnoob <yeahnoob@users.noreply.github.com>
2023-03-14 14:31:12 +0800

    use papercolor scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,30 @@
 return {
-  -- add gruvbox & papercolor
+  -- Noobs' favorit dark colorscheme
   { "lifepillar/vim-gruvbox8" },
-  { "gongxgong/papercolor-theme" },
+  -- Noobs' working colorscheme, for the hometown Chengdu
   { "yeahnoob/chengdu-west.nvim" },
+  -- colorscheme in journey
+  {
+    "Mofiqul/vscode.nvim",
+    opts = function()
+      local c = require('vscode.colors').get_colors()
+      return {
+        style="light",
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+        },
+      }
+    end
+  },
+  { "https://gitlab.com/yorickpeterse/happy_hacking.vim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "chengdu-west-moon",
+      colorscheme = "vscode",
     },
   },
 }
