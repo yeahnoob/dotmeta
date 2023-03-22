416be4a935be77dc76de0fee2c81bd0fb65ec21c
Xanadu Zhang <3369760+yeahnoob@users.noreply.github.com>
2023-03-23 00:14:18 +0800

    update UI starter

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,27 +4,14 @@ return {
   -- Noobs' working colorscheme, for the hometown Chengdu
   { "yeahnoob/chengdu-west.nvim" },
   -- colorscheme in journey
-  {
-    "Mofiqul/vscode.nvim",
-    opts = function()
-      local c = require('vscode.colors').get_colors()
-      return {
-        style="light",
-        group_overrides = {
-          -- this supports the same val table as vim.api.nvim_set_hl
-          -- use colors from this colorscheme by requiring vscode.colors!
-          Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-        },
-      }
-    end
-  },
+  { "NLKNguyen/papercolor-theme" },
   { "https://gitlab.com/yorickpeterse/happy_hacking.vim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vscode",
+      colorscheme = "chengdu-west-moon",
     },
   },
 }
