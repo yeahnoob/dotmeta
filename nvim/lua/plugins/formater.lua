de83c0c9eafb6f069f78e2e96e54a664b6485a6e
Zhang, Gong <zhang.gong@siemens.com>
2024-02-20 11:30:52 +0800

    Feb 2024 - update config for neovim

--- nvim/lua/plugins/formater.lua
+++ nvim/lua/plugins/formater.lua
@@ -1,54 +1,6 @@
-local M = {}
-
 return {
   "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = true,
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>cF",
-      function()
-        require("conform").format({ formatters = { "injected" } })
-      end,
-      mode = { "n", "v" },
-      desc = "Format Injected Langs",
-    },
-  },
-  init = function()
-    local Util = require("lazy.util")
-    -- Install the conform formatter on VeryLazy
-    require("lazyvim.util").on_very_lazy(function()
-      require("lazyvim.util").format.register({
-        name = "conform.nvim",
-        priority = 100,
-        primary = true,
-        format = function(buf)
-          local plugin = require("lazy.core.config").plugins["conform.nvim"]
-          local Plugin = require("lazy.core.plugin")
-          local opts = Plugin.values(plugin, "opts", false)
-          require("conform").format(Util.merge(opts.format, { bufnr = buf }))
-        end,
-        sources = function(buf)
-          local ret = require("conform").list_formatters(buf)
-          ---@param v conform.FormatterInfo
-          return vim.tbl_map(function(v)
-            return v.name
-          end, ret)
-        end,
-      })
-    end)
-  end,
   opts = function()
-    local Util = require("lazy.util")
-    local plugin = require("lazy.core.config").plugins["conform.nvim"]
-    if plugin.config ~= M.setup then
-      Util.error({
-        "Don't set `plugin.config` for `conform.nvim`.\n",
-        "This will break **LazyVim** formatting.\n",
-        "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
-      }, { title = "LazyVim" })
-    end
     ---@class ConformOpts
     local opts = {
       -- LazyVim will use these options when formatting with the conform.nvim formatter
@@ -84,5 +36,4 @@ return {
     }
     return opts
   end,
-  config = M.setup,
 }
